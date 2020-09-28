917592fee17b306d330f23612ec2acea280b1b25
Afonso Machado <afonso777@hotmail.com>
2020-09-28 15:25:10 -0300

    estruturação basica da pagina following

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-import { Container } from './styles';
+import { Container, Wrapper, Main } from './styles';
 
 const Following: React.FC = () => {
   return (
-    <Container>
-      <Text>Following</Text>
-    </Container>
+    <Wrapper>
+      <Container>
+        <Main />
+      </Container>
+    </Wrapper>
   );
 };
 
