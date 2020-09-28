c9e769ef4778716e97a6321992f04c6b9f3df97b
Afonso Machado <afonso777@hotmail.com>
2020-09-28 17:22:26 -0300

    estilização inicial do steam list

---
+++ src/components/StreamList/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const List = styled.View`
+  padding: 8px 0 24px;
+`;
+
+export const StreamContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  flex: 1;
+
+  margin-bottom: 24px;
+  margin-right: 24px;
+`;
+
+export const StreamColumn = styled.View``;
+
+export const StreamThumbnail = styled.Image`
+  width: 119px;
+  height: 66px;
+`;
+
+export const StreamRow = styled.View``;
+
+export const StreamHeader = styled.View``;
+
+export const StreamAvatar = styled.View``;
+
+export const StreamUsername = styled.Text``;
+
+export const StreamDescription = styled.Text``;
+
+export const StreamCategory = styled.Text``;
+
+export const TagRow = styled.View``;
+
+export const TagView = styled.View``;
+
+export const TagText = styled.Text``;
